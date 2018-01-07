f65259fdc02976336134ae8f25ad097718b19747
SwaggersaurusRex <lee.jeremy1@gmail.com>
2018-01-06 22:23:25 -0800

    Implemented tests for Menu page

--- src/test/java/Menu.java
+++ src/test/java/Menu.java
@@ -1,2 +1,70 @@
-public class Menu {
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Menu
+{
+    private static WebDriver driver;
+    private static PracticeSelenium page;
+
+    @BeforeClass
+    public static void setUp()
+    {
+        page = new PracticeSelenium();
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.get("http://www.practiceselenium.com/");
+
+        driver.findElement(By.linkText("Menu")).click();
+    }
+
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void checkTitle()
+    {
+        String title = driver.getTitle();
+
+        Assert.assertEquals(true, title.contains("Menu"));
+    }
+
+    @Test
+    public void checkWelcome()
+    {
+        page.checkWelcome(driver);
+    }
+
+    @Test
+    public void checkOurPassion()
+    {
+        page.checkOurPassion(driver);
+    }
+
+    @Test
+    public void checkMenu()
+    {
+        page.checkMenu(driver);
+    }
+
+    @Test
+    public void checkLetsTalkTea()
+    {
+        page.checkLetsTalkTea(driver);
+    }
+
+    @Test
+    public void checkCheckOut()
+    {
+        page.checkCheckOut(driver);
+    }
+
 }
--- src/test/java/Welcome.java
+++ src/test/java/Welcome.java
@@ -2,7 +2,6 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
